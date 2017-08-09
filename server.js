103fe6e71a56ef9bde9e46cbdf1adf6c15e94d73
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-08 19:59:44 -0600

    90 percent done, a few things to clean

--- server.js
+++ server.js
@@ -4,8 +4,50 @@ var app = express();
 
 app.use(express.static('./public'));
 
+/* CHICAGO/ HOME */
 app.get('/chicago', function (req, res) {
     res.sendFile('./index.html', {root:'./public'});
+});
+
+/* BOSTON */
+app.post('/boston', function (req, res) {
+    res.redirect('/boston.html');
+});
+
+app.get('/boston', function (req, res) {
+    res.sendFile('./boston.html', {root:'/public'});
+});
+
+/* NORTH CAR */
+app.post('/northCar', function (req, res) {
+    res.redirect('/northCar.html');
+});
+
+app.get('/northCar', function (req, res) {
+    res.sendFile('./northCar.html', {root:'/public'});
+});
+
+/* DELAWARE */
+app.post('/delaware', function (req, res) {
+    res.redirect('/delaware.html');
+});
+
+app.get('/delaware', function (req, res) {
+    res.sendFile('./delaware.html', {root:'/public'});
+});
+
+/* DENVER */
+app.post('/denver', function (req, res) {
+    res.redirect('/denver.html');
+});
+
+app.get('/denver', function (req, res) {
+    res.sendFile('./denver.html', {root:'/public'});
+});
+
+/* HOME BUTTON */
+app.post('/chicago', function (req, res) {
+    res.redirect('/index.html');
 });
 
 app.listen(8000);
